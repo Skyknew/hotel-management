7bd0a11cea50a730cd60e91e0209a87917bb70cd
Aakash Thapa <aakashthapa38@gmail.com>
2024-01-12 00:32:26 +0545

    adder header components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
+import Header from '@/components/Header/Header';
 
 const poppins = Poppins({ 
   subsets: ['latin'], 
@@ -21,7 +22,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        < Header />
+        {children}
+        {/* ?Footer */}
+      </body>
     </html>
   )
 }
