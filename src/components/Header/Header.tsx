6a5731c08d6be157775ea3e74dac4fbcf77926fd
Aakash Thapa <aakashthapa38@gmail.com>
2024-01-19 22:52:27 +0545

    Link added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between ">
         <div className="flex items-center w-full md:2/3 ">
         </div>
-
+          <Link href="/"></Link>
         <ul></ul>
     </header>
   )
