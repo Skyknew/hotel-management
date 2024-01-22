54cb344a58bebdac8c76eb990ac20f6e9c0fce65
Aakash Thapa <aakashthapa38@gmail.com>
2024-01-22 23:28:32 +0545

    header given

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,13 @@ const Header = () => {
     <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between ">
         <div className="flex items-center w-full md:2/3 ">
         </div>
-          <Link href="/"></Link>
-        <ul></ul>
+          <Link href="/" className="font-black text-[#F27405]">
+            Hotelzz
+          </Link>
+
+        <ul className="flex items-center ml-5">
+
+        </ul>
     </header>
   )
 }
