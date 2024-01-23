85355ec739840c45916173e946916e1f43a12eb7
Aakash Thapa <aakashthapa38@gmail.com>
2024-01-23 22:37:56 +0545

    added fausercircle n mddarkmode icons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { FaUserCircle } from "react-icons/fa";
+import { MdDarkMode } from "react-icons/md";
 
 const Header = () => {
   return (
@@ -10,7 +12,15 @@ const Header = () => {
           </Link>
 
         <ul className="flex items-center ml-5">
+          <li className="flex items-center">
+          <Link href="/auth">
+            <FaUserCircle className="cursor-pointer" />
+          </Link>
+          </li>
 
+          <li className="ml-2">
+            <MdDarkMode/>
+          </li>
         </ul>
     </header>
   )
